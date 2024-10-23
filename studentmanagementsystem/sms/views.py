961eaa4161c2e4e44e00ac90b26065ab9337d1a4
Alen joy <alen85292@gmail.com>
2024-10-23 13:39:16 +0530

    started

---
+++ studentmanagementsystem/sms/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from .models import *
+std1=[]
+# Create your views here.
+def disp(req):
+    data=student.objects.all()
+    return render(req,'display_std.html',{'data':data})
+
+# def std_form(req):
+#     if req.method=='POST':
+#         id=req.POST['id']
+#         Task=req.POST['Task']
+#         std1.append({'id':id,'Task':Task})
+#         print(std1)
+#         return redirect(std1)
+#     return render(req,"todo.html",{'todo':std1})
