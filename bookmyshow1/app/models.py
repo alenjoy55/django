3e10776c5ab619addfba002a6c04916c2598df99
alenjoy55 <alen85292@gmail.com>
2024-11-07 13:33:16 +0530

    update

--- bookmyshow1/app/models.py
+++ bookmyshow1/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    movie_name=models.TextField()
+    bg_img=models.FileField()
+    fg_img=models.FileField()
+    time_duration=models.TextField()
+    category=models.TextField()
+    date=models.DateField()
+
+class lang(models.Model):
+    language=models.TextField()
+
+class movie_lang(models.Model):
+    movie=models.ForeignKey(Movie,on_delete=models.CASCADE)
+    lang=models.ForeignKey(lang,on_delete=models.CASCADE)
